e533f0c9b935a9dccc6403dae8ab77db762c0bf0
Jordqn <57017724+Xari0x@users.noreply.github.com>
2024-06-06 14:45:06 +0200

    Upload

---
+++ Purple/Light/colored_map/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+data_file "SCALEFORM_DLC_FILE" "stream/int3232302352.gfx"
+
+files {
+  "stream/int3232302352.gfx"
+}
+
+client_script 'client.lua'
